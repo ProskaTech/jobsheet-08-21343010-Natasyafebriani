dfeba8210514b754e7be0b05fcdbddb08b116f68
21343010-Natasyafebriani <112762485+21343010-Natasyafebriani@users.noreply.github.com>
2022-10-09 21:06:23 +0700

    Add files via upload

---
+++ Jobsheet8/src/jobsheet8/ArraySample3.java
@@ -0,0 +1,20 @@
+/*
+ Created By-21343010-Natasya febriani
+ */
+
+//Percobaan 3 array multidimensi
+package jobsheet8;
+
+public class ArraySample3 {
+    public static void main(String args[]){
+        
+        //String array 4 baris x 2 kolom
+        String[][] dogs = {{ "Terry", "brown"}, //baris ke 0
+                           { "Kristin", "white"}, //baris ke 1
+                           { "Tobby", "gray"}, //baris ke 2
+                           { "Fido", "black"}, //baris ke 3
+                          };
+        System.out.println(dogs[0][1]);
+        //mengakses variabel dogs dengan indeks baris 0, indeks kolom 0
+        }
+    }  
---
+++ Jobsheet8/src/jobsheet8/Tugas1.java
@@ -0,0 +1,14 @@
+/*
+ Created By 21343010-Natasya febriani
+ */
+package jobsheet8;
+
+public class Tugas1 {
+    public static void main(String[] args){
+        String[] days = {"Monday","Tuesday","Wednesday","Thursday", "Friday","Saturday","Sunday"};
+        
+        for (int i= 0; i< days.length; i++)
+        System.out.println(days[i]);
+            
+    }
+}
